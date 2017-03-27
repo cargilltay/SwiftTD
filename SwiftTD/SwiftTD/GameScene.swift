6d4c481c6ec98146af42a38c12fd11c8bcbd6814
Taylor Cargill <cargillt@mail.gvsu.edu>
2017-03-27 15:40:08 -0400

    attempt at dragging tower to grid. Not detecting currently

--- SwiftTD/SwiftTD/GameScene.swift
+++ SwiftTD/SwiftTD/GameScene.swift
@@ -11,7 +11,9 @@ import GameplayKit
 
 class GameScene: SKScene {
     let game: GameController = GameController()
+    let TowerButtonName = "Rock"
     static let defaultScale: CGFloat = 0.0625
+    var isFingerOnTower = false
     
     override func didMove(to: SKView) {
         game.setup()
@@ -20,6 +22,40 @@ class GameScene: SKScene {
         
         drawMonsters()
         
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        let touchLocation = touch!.location(in: self)
+        
+        if let body = physicsWorld.body(at: touchLocation) {
+            //start here: Not detecting for some reason
+            print(body.node!.name ?? "none")
+            if body.node!.name == TowerButtonName {
+                print("Began touch on paddle")
+                isFingerOnTower = true
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isFingerOnTower {
+            let touch = touches.first
+            let touchLocation = touch!.location(in: self)
+            let previousLocation = touch!.previousLocation(in: self)
+            
+            let tower = childNode(withName: TowerButtonName) as! SKSpriteNode
+            
+            //create towerX
+            var towerX = tower.position.x + (touchLocation.x - previousLocation.x)
+            
+            //assign towerX
+            towerX = max(towerX, tower.size.width/2)
+            towerX = min(towerX, size.width - tower.size.width/2)
+            
+            //set position during drag
+            tower.position = CGPoint(x: towerX, y: tower.position.y)
+        }
     }
     
     func drawMonsters(){
---
+++ SwiftTD/SwiftTD/Views/SwiftTDTowerButton.swift
@@ -0,0 +1,22 @@
+//
+//  TowerButton.swift
+//  SwiftTD
+//
+//  Created by Taylor Cargill on 3/27/17.
+//  Copyright © 2017 Taylor Cargill. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SwiftTDTowerButton: SKSpriteNode {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
