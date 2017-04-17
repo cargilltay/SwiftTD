d9932e1fad71c389a79bbd480f74876bb2cfadcd
Taylor Cargill <cargillt@mail.gvsu.edu>
2017-04-17 01:00:15 -0400

    testing new sprite

---
+++ SwiftTD/SwiftTD/Controllers/SoundEffectController.swift
@@ -0,0 +1,13 @@
+//
+//  SoundEffectController.swift
+//  SwiftTD
+//
+//  Created by Taylor Cargill on 4/16/17.
+//  Copyright © 2017 Taylor Cargill. All rights reserved.
+//
+
+import Foundation
+
+class SoundEffectController{
+    
+}
