a142f8c5841d661cea8911cfc504c848f039b6ef
Taylor Cargill <cargillt@mail.gvsu.edu>
2017-04-20 21:53:06 -0400

    initial social media integration. Need to delegate game data over to it stilll

--- SwiftTD/SwiftTD/Views/GameOverModal.swift
+++ SwiftTD/SwiftTD/Views/GameOverModal.swift
@@ -8,8 +8,79 @@
 
 import Foundation
 import UIKit
+import Social
 
 class GameOverModal: UIViewController {
+    
+    @IBAction func showShareOptions(_ sender: Any) {
+        //need to dismiss the popover
+        
+        let actionSheet = UIAlertController(title: "", message: "Share your Note", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        
+            // Configure a new action for sharing the note in Twitter.
+        let tweetAction = UIAlertAction(title: "Share on Twitter", style: UIAlertActionStyle.default) { (action) -> Void in
+            // Check if sharing to Twitter is possible.
+            if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+                // Initialize the default view controller for sharing the post.
+                let twitterComposeVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+                
+                // Set the score as the default post message.
+                twitterComposeVC?.setInitialText("HELLO WORLD")
+                    
+                self.present(twitterComposeVC!, animated: true, completion: nil)
+            }
+            else {
+                self.showAlertMessage(message: "You are not logged in to your Twitter account.")
+            }
+        }
+        
+        
+        // Configure a new action to share on Facebook.
+        let facebookPostAction = UIAlertAction(title: "Share on Facebook", style: UIAlertActionStyle.default) { (action) -> Void in
+            if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeFacebook) {
+                let facebookComposeVC = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+                
+                facebookComposeVC?.setInitialText("HELLO WORLD")
+                
+                self.present(facebookComposeVC!, animated: true, completion: nil)
+            }
+            else {
+                self.showAlertMessage(message: "You are not connected to your Facebook account.")
+            }
+        }
+        
+        // Configure a new action to show the UIActivityViewController
+        let moreAction = UIAlertAction(title: "More", style: UIAlertActionStyle.default) { (action) -> Void in
+            let activityViewController = UIActivityViewController(activityItems: ["HELLO WORLD"], applicationActivities: nil)
+            
+            activityViewController.excludedActivityTypes = [UIActivityType.mail]
+            
+            self.present(activityViewController, animated: true, completion: nil)
+
+        }
+        
+        
+        let dismissAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (action) -> Void in
+            
+        }
+        
+        
+        actionSheet.addAction(tweetAction)
+        actionSheet.addAction(facebookPostAction)
+        actionSheet.addAction(moreAction)
+        actionSheet.addAction(dismissAction)
+        
+        present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    func showAlertMessage(message: String!) {
+        let alertController = UIAlertController(title: "EasyShare", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         
